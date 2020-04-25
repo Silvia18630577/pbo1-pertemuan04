8dbb404023f717337e9f47f16d7a426b2d86573a
Silvia18630577 <62637906+Silvia18630577@users.noreply.github.com>
2020-04-25 13:19:14 +0800

    Array Matakuliah

---
+++ src/Array/Matakuliah.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Matakuliah {
+    public static void main(String[] args) {
+        String Matakuliah[];
+        Matakuliah = new String[10];
+
+        Matakuliah[0] = "Pemrograman Berbasis Objek 1";
+        Matakuliah[1] = "Pemrograman Web 2";
+        Matakuliah[2] = "Sistem Informasi Geografis";
+        Matakuliah[3] = "Perancangan Database";
+        Matakuliah[4] = "Tauhid";
+    }
+}
